95fc65530ade75afd6dea20863fd0bda6412c4bc
Zrossiz <soon.pluxury@mail.ru>
2024-05-23 15:22:50 +0300

    feat: created time to cook model

--- time_to_cook/models.py
+++ time_to_cook/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TimeToCook(models.Model):
+    time = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'time_to_cook'
