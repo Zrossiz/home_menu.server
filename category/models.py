f6abdd0630881fbb5486a5f83177e77243e4fa10
Zrossiz <soon.pluxury@mail.ru>
2024-05-23 15:18:04 +0300

    feat: created category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=300)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'category'
